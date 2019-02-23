3c420ca9964d214ca58fb9e2f4d0b86f6e19e3dd
Sebastian Bevc <sebastian.bevc@gmail.com>
2019-02-23 12:10:46 -0300

    completed ch5

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,14 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    @property
+    def inputbox(self):
+        return self.browser.find_element_by_id('id_new_item')
+
+    def enter_new_todo(self, text):
+        self.inputbox.send_keys(text + Keys.ENTER)
+        time.sleep(1)
+
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
@@ -29,29 +37,20 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.get_attribute('placeholder'),
+            self.inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
-        # is tying fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')
-
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
+        # is tying fly-fishing lures). When she hits enter, the page updates,
+        # and now the page lists "1: Buy peacock feathers" as an item in a to-do list
+        self.enter_new_todo('Buy peacock feathers')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.enter_new_todo('Use peacock feathers to make a fly')
 
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
