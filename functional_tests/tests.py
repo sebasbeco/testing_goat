7ed79cc02234423daa6ea8089863dbf0dc318362
Sebastian Bevc <sebastian.bevc@gmail.com>
2019-02-24 09:43:12 -0300

    use bootstrap

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,6 @@
 import time
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
@@ -9,7 +9,7 @@ chromedriver = r'/home/sebastian/chromedriver/chromedriver'
 MAX_WAIT = 1
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome(chromedriver)
